e4853fe6963a784cf4f97e7c183dc64488d8d3fa
현준 <rlaguswns95@haru-tokyo.com>
2025-12-03 00:13:40 +0900

    주소 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/booking-manager/', // ★ 중요: 깃허브 저장소 이름과 똑같이 적어야 합니다
+  base: '/booking-manager/', // ★ 깃허브 저장소 이름과 똑같아야 합니다
 })
